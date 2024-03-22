4a413aebca6824c595f5b0fd7286155cbbd615d6
Sven <scysys@outlook.com>
2024-03-22 17:54:25 +0100

    0.9.8

--- start.sh
+++ start.sh
@@ -9,5 +9,9 @@ rm -f /home/spcast/SPCast/jingles/logs/*.log*
 # Set permissions
 chmod +x spcast_jingles.liq
 
+# Replace the value in jingles_credentials_alternate.liq
+source_password=$(cat /usr/share/zabbix/sp/secure/autodj_source_password.txt.php)
+sed -i "s/spChangeToAutoDJSourcePassword/$source_password/" /home/spcast/SPCast/jingles/jingles_credentials_alternate.liq
+
 # Execute the spcast_jingles.liq with the name aotonhwwouqvhw
 bash -c './spcast_jingles.liq --name "aotonhwwouqvhw"' > /dev/null 2>&1
