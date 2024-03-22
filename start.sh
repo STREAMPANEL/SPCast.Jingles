3f5f452bdf257a6de3ae289261bef6c92cf12f13
Sven <scysys@outlook.com>
2024-03-22 17:28:11 +0100

    0.9.8

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ rm -f /home/spcast/SPCast/jingles/logs/*.log*
 chmod +x spcast_jingles.liq
 
 # Execute the spcast_jingles.liq with the name aotonhwwouqvhw
-bash -c './spcast_jingles.liq --name "aotonhwwouqvhw"'
+bash -c './spcast_jingles.liq --name "aotonhwwouqvhw"' > /dev/null 2>&1
